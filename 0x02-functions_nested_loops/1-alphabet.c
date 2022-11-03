8d34e471dbbf6b5446c399772df919e30fb2b15b
NDUWAYEZU Placide <ndiplacide7@gmail.com>
2022-11-03 03:03:55 -0700

    1. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game ==> a function that prints the alphabet, in lowercase, followed by a new line.

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,18 @@
-/*
- * File: 1-alphabet.c
- * Auth: Placide
- */
-
-#include <stdio.h>
-
 /**
- * print_alphabet - prints the alphabet in lowercase, followed by a new line.
- */
+* print_alphabet - Entry point
+* Description: a function that prints the alphabet,
+* in lowecase followed by a new line
+* Return: void
+*/
 void print_alphabet(void)
 {
-	char letter;
+	char letter = 'a';
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-		putchar(letter);
-
-	putchar('\n');
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
 }
+
