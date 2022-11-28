6cf2f9393b9b3b2305e40cbf31e8b4dce243b875
NDUWAYEZU Placide <ndiplacide7@gmail.com>
2022-11-27 21:25:12 -0800

    Al in One

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,7 @@
 #include "main.h"
 /**
  * print_aphabet - a function that prints the alphabet, in lower case
- *
- * Description: A function that print the alphabet in lowercase
- *
+ * Description : 'A function that print the alphabet in lowercase'
  * Return : Always 0 (Succes)
  */
 void print_alphabet(void)
